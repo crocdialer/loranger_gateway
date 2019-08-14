9e0002bd6e9e33b0970a76ea99eb43a9bac05f5e
crocdialer <crocdialer@googlemail.com>
2019-08-14 21:16:53 +0200

    all radiohead and bcm shit compiling

--- src/bcm2835.c
+++ src/bcm2835.c
@@ -630,9 +630,9 @@ void bcm2835_gpio_set_pud(uint8_t pin, uint8_t pud)
     } else
     {
     bcm2835_gpio_pud(pud);
-    delayMicroseconds(10);
+    bcm2835_delayMicroseconds(10);
     bcm2835_gpio_pudclk(pin, 1);
-    delayMicroseconds(10);
+    bcm2835_delayMicroseconds(10);
     bcm2835_gpio_pud(BCM2835_GPIO_PUD_OFF);
     bcm2835_gpio_pudclk(pin, 0);
 }

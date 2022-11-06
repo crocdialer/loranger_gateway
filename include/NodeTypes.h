21df0043901a3383a3bb50559afe49711b012f92
crocdialer <crocdialer@gmail.com>
2022-11-06 16:05:32 +0100

    cleanup

--- include/NodeTypes.h
+++ include/NodeTypes.h
@@ -8,8 +8,6 @@
 #define STRUCT_TYPE_ELEVATOR_CONTROL 0x13
 #define STRUCT_TYPE_TEMPERATUREMAN 0x76
 #define STRUCT_TYPE_WEATHERMAN 0x77
-#define STRUCT_TYPE_GASMAN 0xF0
-#define STRUCT_TYPE_GASWEATHERBOY 0xF1
 #define STRUCT_TYPE_TRACKERMAN 0x66
 #define STRUCT_TYPE_RADIOSTROM_3000 0x33
 
@@ -66,45 +64,6 @@ typedef struct weather_t
     uint8_t humidity = 0;
 
 } weather_t;
-
-typedef struct gasman_t
-{
-    uint8_t stype = STRUCT_TYPE_GASMAN;
-
-    // battery in range [0 .. 1]
-    uint8_t battery = 0;
-
-    // eCO2 (equivalent calculated carbon-dioxide) concentration in parts per million (ppm)
-    uint16_t eco2 = 0;
-
-    // TVOC (total volatile organic compound) concentration in parts per billion (ppb)
-    uint16_t tvoc = 0;
-
-} gasman_t;
-
-typedef struct gasweatherboy_t
-{
-    uint8_t stype = STRUCT_TYPE_GASWEATHERBOY;
-
-    // battery in range [0 .. 1]
-    uint8_t battery = 0;
-
-    //! temperature in range [-50C .. 100C]
-    uint16_t temperature = 0;
-
-    //! pressure in range [0hPa .. 2000hPa]
-    uint16_t pressure = 0;
-
-    // relative humidity in range [0..1]
-    uint8_t humidity = 0;
-
-    // eCO2 (equivalent calculated carbon-dioxide) concentration in range [400 .. 8192] parts per million (ppm)
-    uint16_t eco2 = 0;
-
-    // TVOC (total volatile organic compound) concentration in range [0 .. 1187] parts per billion (ppb)
-    uint16_t tvoc = 0;
-
-} gasweatherboy_t;
 
 typedef struct trackerman_t
 {
